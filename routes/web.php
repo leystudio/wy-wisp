d6be855b92f61c3b6b471902f65c12624f558d2e
leystudio <leystudio>
2022-12-26 20:10:34 -0400

    WEB

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//subir
+//subir8
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
