25f9687387e0983fd08cd6fd10a869760d3e1866
leystudio <leystudio>
2023-01-02 16:57:42 -0400

    mas archivos

---
+++ resources/views/vendor/jetstream/components/label_empresa.blade.php
@@ -0,0 +1,13 @@
+<div class="card-body">
+    <a href="#">
+      <h4 class="font-semibold">Material Design 3</h4>
+    </a>
+    <p class="opcacity-60 mb-4">
+      The time is now for it to be okay to be great. People in this
+      world shun people for being great. For being a bright color. For
+      standing out.
+    </p>
+    <button class="button button-pink" data-ripple-light="true">
+      Read More
+    </button>
+  </div>
